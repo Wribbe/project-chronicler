8dd9ef3a4654b59f30ad09ffd9082689b378099c
Stefan Eng <eng_steff@outlook.com>
2021-01-11 13:07:35 +0100

    Use the correct socket.

--- rpgchronicler/static/js/ping.js
+++ rpgchronicler/static/js/ping.js
@@ -1,2 +1,2 @@
-const url_socket = process.env.ENV_URL || "ws://localhost:8000"
+const url_socket = process.env.ENV_RPGCHRONICLER_URL || "ws://localhost:8000"
 const socket = new WebSocket(url_socket);
